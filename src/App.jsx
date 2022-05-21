967fb49db8fa3eb974a945a1be6e6ad9d2910058
Sagyam Thapa <sagyamthapa32@gmail.com>
2022-05-21 09:34:15 +0545

    Setup Complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+    return (
+        <>
+            <h1>Hello</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+                magnam. Sapiente aspernatur cum, inventore nihil nam a? Tempora
+                molestiae voluptatibus esse, error amet possimus ea distinctio
+                pariatur delectus, sit aliquid!
+            </p>
+        </>
+    )
+}
+
+export default App
